2322f4674e280722f110c5f9de8b3c658be68339
Steven Zafrani <zsteven44@gmail.com>
2018-09-01 12:57:52 -0700

    changed file structure

---
+++ app/src/main/java/com/zsteven44/android/myrxjavaproject/imgurfragment/ImgurService.java
@@ -0,0 +1,22 @@
+package com.zsteven44.android.myrxjavaproject.imgurfragment;
+
+import com.zsteven44.android.myrxjavaproject.imgurfragment.imgur.ImgurGalleryList;
+
+import io.reactivex.Flowable;
+import retrofit2.Response;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface ImgurService {
+
+    @Headers("Authorization: Client-ID 28710bba9a6f38c")
+    @GET("/3/gallery/search/{sort}/{window}/{page}")
+    Flowable<Response<ImgurGalleryList>> getSearchGallery(@Path("sort")String sort,
+                                                         @Path("window")String window,
+                                                         @Path("page")int page,
+                                                         @Query("q")String search);
+
+
+}
---
+++ app/src/main/java/com/zsteven44/android/myrxjavaproject/imgurfragment/imgur/ImgurItem.java
@@ -0,0 +1,91 @@
+package com.zsteven44.android.myrxjavaproject.imgurfragment.imgur;
+
+public abstract class ImgurItem {
+
+    private String id;
+    private String title;
+    private String description;
+    private String link;
+    private int ups;
+    private int downs;
+    private int views;
+
+    public ImgurItem(String id, String title, String description, String link, int ups, int downs, int views) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.link = link;
+        this.ups = ups;
+        this.downs = downs;
+        this.views = views;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public int getUps() {
+        return ups;
+    }
+
+    public void setUps(int ups) {
+        this.ups = ups;
+    }
+
+    public int getDowns() {
+        return downs;
+    }
+
+    public void setDowns(int downs) {
+        this.downs = downs;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    @Override
+    public String toString() {
+        return "ImgurItem{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", link='" + link + '\'' +
+                ", ups=" + ups +
+                ", downs=" + downs +
+                ", views=" + views +
+                '}';
+    }
+}
